fda9a9b9bc7c724c2a2ef94055dc485ded7c942f
Grant McLean <grant@mclean.net.nz>
2009-04-02 23:37:26 +1300

    implement support for UTF-8 passthru and Latin-1/Win-Latin-1 conversions

--- lib/Encoding/FixLatin.pm
+++ lib/Encoding/FixLatin.pm
@@ -12,11 +12,86 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(fix_latin);
 
 
+my $byte_map;
+
+my $ascii_char = '[\x00-\x7F]';
+my $cont_byte  = '[\x80-\xBF]';
+
+my $utf8_2     = '[\xC0-\xDF]' . $cont_byte;
+my $utf8_3     = '[\xE0-\xEF]' . $cont_byte . '{2}';
+my $utf8_4     = '[\xF0-\xF7]' . $cont_byte . '{3}';
+my $utf8_5     = '[\xF8-\xFB]' . $cont_byte . '{4}';
+
+my $nibble_good_chars = qr{^($ascii_char+|$utf8_2|$utf8_3|$utf8_4|$utf8_5)(.*)$}s;
+
 sub fix_latin {
     my($input) = @_;
 
     return unless defined($input);
-    return $input;
+    _init_byte_map() unless $byte_map;
+
+    my $output = '';
+    my $char   = '';
+    my $rest   = '';
+    while(length($input) > 0) {
+        if(($char, $rest) = $input =~ $nibble_good_chars) {
+            $output .= $char;
+        }
+        else {
+            ($char, $rest) = $input =~ /^(.)(.*)$/s;
+            $output .= $byte_map->{$char};
+        }
+        $input = $rest;
+    }
+    utf8::decode($output);
+    return $output;
+}
+
+
+sub _init_byte_map {
+    foreach my $i (0x80..0xFF) {
+        my $utf_char = chr($i);
+        utf8::encode($utf_char);
+        $byte_map->{pack('C', $i)} = $utf_char;
+    }
+    _add_cp1252_mappings();
+}
+
+
+sub _add_cp1252_mappings {
+    # From http://unicode.org/Public/MAPPINGS/VENDORS/MICSFT/WINDOWS/CP1252.TXT
+    my %ms_map = (
+        "\x80" => "\xE2\x82\xAC",  # EURO SIGN
+        "\x82" => "\xE2\x80\x9A",  # SINGLE LOW-9 QUOTATION MARK
+        "\x83" => "\xC6\x92",      # LATIN SMALL LETTER F WITH HOOK
+        "\x84" => "\xE2\x80\x9E",  # DOUBLE LOW-9 QUOTATION MARK
+        "\x85" => "\xE2\x80\xA6",  # HORIZONTAL ELLIPSIS
+        "\x86" => "\xE2\x80\xA0",  # DAGGER
+        "\x87" => "\xE2\x80\xA1",  # DOUBLE DAGGER
+        "\x88" => "\xCB\x86",      # MODIFIER LETTER CIRCUMFLEX ACCENT
+        "\x89" => "\xE2\x80\xB0",  # PER MILLE SIGN
+        "\x8A" => "\xC5\xA0",      # LATIN CAPITAL LETTER S WITH CARON
+        "\x8B" => "\xE2\x80\xB9",  # SINGLE LEFT-POINTING ANGLE QUOTATION MARK
+        "\x8C" => "\xC5\x92",      # LATIN CAPITAL LIGATURE OE
+        "\x8E" => "\xC5\xBD",      # LATIN CAPITAL LETTER Z WITH CARON
+        "\x91" => "\xE2\x80\x98",  # LEFT SINGLE QUOTATION MARK
+        "\x92" => "\xE2\x80\x99",  # RIGHT SINGLE QUOTATION MARK
+        "\x93" => "\xE2\x80\x9C",  # LEFT DOUBLE QUOTATION MARK
+        "\x94" => "\xE2\x80\x9D",  # RIGHT DOUBLE QUOTATION MARK
+        "\x95" => "\xE2\x80\xA2",  # BULLET
+        "\x96" => "\xE2\x80\x93",  # EN DASH
+        "\x97" => "\xE2\x80\x94",  # EM DASH
+        "\x98" => "\xCB\x9C",      # SMALL TILDE
+        "\x99" => "\xE2\x84\xA2",  # TRADE MARK SIGN
+        "\x9A" => "\xC5\xA1",      # LATIN SMALL LETTER S WITH CARON
+        "\x9B" => "\xE2\x80\xBA",  # SINGLE RIGHT-POINTING ANGLE QUOTATION MARK
+        "\x9C" => "\xC5\x93",      # LATIN SMALL LIGATURE OE
+        "\x9E" => "\xC5\xBE",      # LATIN SMALL LETTER Z WITH CARON
+        "\x9F" => "\xC5\xB8",      # LATIN CAPITAL LETTER Y WITH DIAERESIS
+    );
+    while(my($k, $v) = each %ms_map) {
+        $byte_map->{$k} = $v;
+    }
 }
 
 
--- t/10-fix_latin.t
+++ t/10-fix_latin.t
@@ -1,6 +1,6 @@
 #!perl -T
 
-use Test::More tests => 4;
+use Test::More tests => 16;
 
 BEGIN {
     use_ok( 'Encoding::FixLatin', 'fix_latin' );
@@ -8,6 +8,39 @@ BEGIN {
 
 ok(__PACKAGE__->can('fix_latin'), 'fix_latin() function was imported on demand');
 
-is(fix_latin(undef), undef, 'undefined input handled correctly');
-is(fix_latin(''), '', 'empty string handled correctly');
+is(fix_latin(undef) => undef, 'undefined input handled correctly');
+is(fix_latin('')    => '',    'empty string handled correctly');
+
+is(fix_latin('abc') => 'abc', 'printable ASCII string handled correctly');
+is(fix_latin("a\nb") => "a\nb", 'embedded newlines cause no problem');
+
+is(fix_latin("\007\011\012") => "\007\011\012",
+    'ASCII control characters passed through');
+
+is(fix_latin("\xC2\xA0") => "\x{A0}",
+    'UTF-8 NBSP passed through');
+
+is(fix_latin("\xA0") => "\x{A0}",
+    'Latin-1 NBSP converted to UTF-8');
+
+is(fix_latin("\xA0\xC2\xA0\xA0") => "\x{A0}\x{A0}\x{A0}",
+    'Mixed UTF-8 and Latin-1 NBSPs passed/converted');
+
+is(fix_latin("Caf\xC3\xA9") => "Caf\x{E9}",
+    'UTF-8 accented character passed through');
+
+is(fix_latin("Caf\xE9") => "Caf\x{E9}",
+    'Latin-1 accented character converted');
+
+is(fix_latin("P\xC3\xA3n Caf\xE9") => "P\x{E3}n Caf\x{E9}",
+    'Mixed UTF-8 and Latin-1 accented characters passed/converted');
+
+is(fix_latin("\xE2\x82\xAC") => "\x{20AC}",
+    'UTF-8 Euro symbol passed through');
+
+is(fix_latin("\x80") => "\x{20AC}",
+    'Win-Latin-1 Euro symbol converted');
+
+is(fix_latin("\x81") => "\x{81}",
+    'Latin-1 control character converted');
 
